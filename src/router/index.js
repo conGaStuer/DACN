af33746736dfed866e280a9c769ec8aa0f492b9a
Khuong Tran <xuankhuong1402@gmail.com>
2023-12-10 17:41:02 +0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/Pages/HomeView.vue";
+import {} from "@/util/loginFunction/loginFunction";
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
+    props: {},
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Pages/AboutView.vue"),
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Pages/ContactView.vue"),
+  },
+  {
+    path: "/shop",
+    name: "shop",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Pages/ShopView.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/Login/LoginView.vue"),
+    props: {},
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("../views/Login/RegisterView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Pages/AboutView.vue
@@ -0,0 +1,11 @@
+<template></template>
+<script>
+import Navbar from "./Navbar.vue";
+import Footer from "./Footer.vue";
+export default {
+  components: { Navbar },
+};
+</script>
+<style>
+@import "../../scss/about.scss";
+</style>
